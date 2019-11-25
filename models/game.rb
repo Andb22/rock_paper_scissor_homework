d0580b160a1937cb4807ca9655a66c52dd4050fa
Andrew Binnie <binnieandrew@gmail.com>
2019-11-25 20:01:48 +0000

    Adding in which player won

--- models/game.rb
+++ models/game.rb
@@ -4,19 +4,25 @@ class Game
     player1 = play1.downcase
     player2 = play2.downcase
     if player1 == 'rock' && player2 == 'paper'
-      return "#{player2} wins"
+      return "Player 2 wins with #{player2}"
     elsif player1 == 'rock' && player2 == 'scissors'
-      return "#{player1} wins"
+      return "Player 1 wins with #{player1}"
     elsif player1 == 'rock' && player2 == 'rock'
       return "It's a draw"
     elsif player1 == 'paper' && player2 == 'paper'
       return "It's a draw"
     elsif player1 == 'paper' && player2 == 'scissors'
-      return "#{player2} wins"
+      return "Player 2 wins with #{player2}"
+    elsif player1 == 'paper' && player2 == 'rock'
+      return "Player 1 wins with #{player1}"
     elsif player1 == 'scissors' && player2 == 'scissors'
       return "It's a draw"
+    elsif player1 == 'scissors' && player2 == 'rock'
+      return "Player 2 wins with #{player2}"
+    elsif player1 == 'scissors' && player2 == 'paper'
+      return "Player 1 wins with #{player1}"
     else
-      return "Please enter rock, paper or scissors"
+      return "Players must enter rock, paper or scissors"
     end
   end
 end
